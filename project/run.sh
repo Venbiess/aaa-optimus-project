dd6581443dbb739f723d86897a485847f327270f
Alekseev Andrey <simpleonus5@mail.ru>
2025-06-08 21:24:15 +0300

    runsh fix

--- project/run.sh
+++ project/run.sh
@@ -1,3 +1,3 @@
-mkdir -p /app/model_weights
+mkdir -p model_weights/
 wget -O model_weights/sam_vit_h_4b8939.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 docker compose up --build
