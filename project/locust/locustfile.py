7e5f709b219ae484d5b31171ad88886ed76ba7f7
Alekseev Andrey <simpleonus5@mail.ru>
2025-06-10 02:50:50 +0300

    locust

---
+++ project/locust/locustfile.py
@@ -0,0 +1,11 @@
+from locust import HttpUser, task, between
+
+
+class ModelUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def test_model(self):
+        with open("test_image.jpg", "rb") as f:
+            files = {"file": ("test_image.jpg", f, "image/jpeg")}
+            self.client.post("/upload", files=files)
