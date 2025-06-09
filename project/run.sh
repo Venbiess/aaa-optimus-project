00685f43d5e79af9e126aaba326f496beeab8dc8
Alekseev Andrey <simpleonus5@mail.ru>
2025-06-10 01:52:09 +0300

    run.sh

--- project/run.sh
+++ project/run.sh
@@ -1,3 +1,4 @@
 mkdir -p model_weights/
 wget -O model_weights/sam_vit_h_4b8939.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
-docker compose up --build
+wget -O model_weights/best_unet_model.pth "https://huggingface.co/Venbiess/best_unet_model_car_segmentation/resolve/main/best_unet_model.pth?download=true"
+# docker compose up --build
